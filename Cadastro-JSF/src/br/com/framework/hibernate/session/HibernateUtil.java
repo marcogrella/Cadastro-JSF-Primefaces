772069d972c14eac7684150003c7b179a37d12f4
Marco Grella <68563265+marcogrella@users.noreply.github.com>
2021-05-13 18:41:50 -0300

    Configurações JNDI do Tomcat
    Configurações do JNDI do Tomcat, configura as variáveis do Hibernate.

---
+++ Cadastro-JSF/src/br/com/framework/hibernate/session/HibernateUtil.java
@@ -0,0 +1,104 @@
+package br.com.framework.hibernate.session;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.SessionFactoryImplementor;
+
+import br.com.framework.implementacao.crud.VariavelConexaoUtil;
+
+/**
+ * Responsável por estabelecer a conexão com o hibernate. Essas configurações
+ * estão descritas na documentação do Tomcat
+ * 
+ */
+
+public class HibernateUtil implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+
+	/* objeto estático para prover a conexão do hibernate */
+	private static SessionFactory sessionFactory = buildSessionFactory();
+
+	/* Responsável por ler o arquivo de configuração hibernate.cfg.xml */
+	private static SessionFactory buildSessionFactory() {
+
+		try {
+
+			/*
+			 * verifica se já existe um session factory, pois deve existir apenas uma
+			 * conexão
+			 */
+			if (sessionFactory == null) {
+				sessionFactory = new Configuration().configure().buildSessionFactory();
+			}
+
+			return sessionFactory;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ExceptionInInitializerError("Erro ao criar a conexão do SessionFactiory");
+		}
+
+	}
+
+	/* métodos auxiliares do hibernate util */
+
+	/* Retorna o session factory corrente */
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	/* Retorna a sessão do SessionFactory */
+	public static Session getCurrentSession() {
+		return getSessionFactory().getCurrentSession();
+	}
+
+	/* Retorna uma nova sessão no SessionFactory */
+	public static Session openSession() {
+
+		/* se não houver chamará o método e criará um session */
+		if (sessionFactory == null) {
+			buildSessionFactory();
+		}
+
+		return sessionFactory.openSession();
+
+	}
+
+	/* Obtem a conexão do provedor de conexões configurado */
+	public static Connection getConnectionProvider() throws SQLException {
+
+		return ((SessionFactoryImplementor) sessionFactory).getConnectionProvider().getConnection();
+
+	}
+
+	/* Connection no Initial Context java:/comp/env/jdbc/datasource */
+	public static Connection getConnection() throws Exception {
+
+		InitialContext context = new InitialContext();
+		DataSource ds = (DataSource) context.lookup(JAVA_COMP_ENV_JDBC_DATA_SOURCE);
+		return ds.getConnection();
+
+	}
+
+	
+	/* DataDource do JNDI Tomcat */ 
+	public DataSource getDataSourceJndi() throws NamingException {
+
+		InitialContext context = new InitialContext();
+		return (DataSource) context.lookup(VariavelConexaoUtil.JAVA_COMP_ENV_JDBC_DATA_SOURCE);
+
+	}
+
+}
---
+++ Cadastro-JSF/src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,14 @@
+package br.com.framework.implementacao.crud;
+
+/* 
+ * Classe criada para gerar o datasource (jndi datasource do Apache TOMCAT) 
+ * e fazer conexão com o banco de dados. 
+ */
+
+
+public class VariavelConexaoUtil {
+
+	
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+	
+}
