a55303e80e55851f45a7f68b3b97eaad4241a3af
Marco Grella <68563265+marcogrella@users.noreply.github.com>
2021-05-26 11:48:09 -0300

    Commit Correção de Bugs
    Correção de bugs.

---
+++ Cadastro-JSF/WebContent/resources/javascript/script.js
@@ -0,0 +1,57 @@
+function logout(contextPath) {
+	
+	var post = 'invalidar_session';
+	
+	$.ajax({
+		type:"POST",
+		url: post
+	}).always(function(resposta) { 
+		document.location = contextPath + '/j_spring_security_logout';
+	});
+	
+}
+
+
+function invalidarSession(context, pagina) {
+	document.location = (context + pagina + ".jsf");
+}
+
+function validarSenhaLogin() {
+
+	j_username = document.getElementById("j_username").value;
+	j_password = document.getElementById("j_password").value;
+
+	if (j_username === null || j_username.trim() === '') {
+		alert("Informe o Login.");
+		$('#j_username').focus();
+		return false;
+	}
+
+	if (j_password === null || j_password.trim() === '') {
+		alert("Informe a Senha.");
+		$('#j_password').focus();
+		return false;
+	}
+
+	return true;
+
+}
+
+
+function abrirMenupop() {
+	$("#menupop").show('slow').mouseleave(function() {
+		fecharMenupop();
+	});
+}
+
+function fecharMenupop() {
+	if ($("#menupop").is(":visible")){
+		$("#menupop").hide("slow");
+	}
+}
+
+function redirecionarPagina(context, pagina) {
+	pagina = pagina + ".jsf";
+	document.location = context + pagina;
+}
+
